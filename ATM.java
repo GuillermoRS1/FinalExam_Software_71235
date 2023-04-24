3677c3462be5743763a52cf31eb14adef4b8f57c
Guillermo Ruiz Senso <113591721+GuillermoRS1@users.noreply.github.com>
2023-04-24 11:22:59 +0100

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,16 @@
+Class ATM();
+
+user user = new user();
+public float balanceAcc;
+public float amount;
+
+public void checkUser(int userId, int userPin){
+}
+public void withdraw(float amount){
+}
+
+public void deposit(float amount){
+}
+
+public void seeBalance(int userID){
+}
